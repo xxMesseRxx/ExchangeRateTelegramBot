e18c92e5f84e13994eb8714f08c51172bdda1f2a
xxMesseRxx <343454640@mail.ru>
2023-04-18 22:03:07 +0500

    Was added enum - "Currency".

---
+++ TelegramBot/Library/Currency.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Library
+{
+	public enum Currency
+	{
+		UAH,
+		AUD,
+		CAD,
+		CZK,
+		DKK,
+		HUF,
+		ILS,
+		JPY,
+		LVL,
+		LTL,
+		NOK,
+		SKK,
+		SEK,
+		CHF,
+		GBP,
+		USD,
+		BYR,
+		EUR,
+		GEL,
+		PLZ
+	}
+}
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+var myBot = new TelegramBotClient("5962347349:AAHOYGIfohV1Rw8AFmnYtkaPvOzLJIQaspg");
+myBot.StartReceiving(HandleUpdateAsync, HandlePollingErrorAsync);
+
+Console.ReadLine();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+	// Only process Message updates: https://core.telegram.org/bots/api#message
+	if (update.Message is not { } message)
+		return;
+	// Only process text messages
+	if (message.Text is not { } messageText)
+		return;
+
+	var chatId = message.Chat.Id;
+
+	Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+	// Echo received message text
+	Message sentMessage = await botClient.SendTextMessageAsync(
+		chatId: chatId,
+		text: "You said:\n" + messageText);
+}
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+	var ErrorMessage = exception switch
+	{
+		ApiRequestException apiRequestException
+			=> $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+		_ => exception.ToString()
+	};
+
+	Console.WriteLine(ErrorMessage);
+	return Task.CompletedTask;
+}
