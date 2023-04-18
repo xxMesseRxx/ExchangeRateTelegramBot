88c913ba9584421742beff90913faf0ffaf74054
xxMesseRxx <343454640@mail.ru>
2023-04-18 22:49:37 +0500

    Was created model: "CerrencyRate"

---
+++ TelegramBot/Model/CurrencyRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Model
+{
+	public class CurrencyRate
+	{
+		public readonly string BaseCurrency;
+		public readonly string Currency;
+		public readonly decimal SaleRateNB;
+		public readonly decimal PurchaseRateNB;
+		public readonly decimal? SaleRate;
+		public readonly decimal? PurchaseRate;
+
+		public CurrencyRate(string baseCurrency, string currency,
+							decimal saleRateNB, decimal purchaseRateNB,
+							decimal? saleRate = null, decimal? purchaseRate = null)
+		{
+			BaseCurrency = baseCurrency;
+			Currency = currency;
+			SaleRateNB = saleRateNB;
+			PurchaseRateNB = purchaseRateNB;
+			SaleRate = saleRate;
+			PurchaseRate = purchaseRate;
+		}
+	}
+}
