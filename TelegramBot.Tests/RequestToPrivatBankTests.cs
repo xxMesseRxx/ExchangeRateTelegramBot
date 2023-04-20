87b799f4a9cbdef60c024d6a8f2f3fe30ab72170
xxMesseRxx <343454640@mail.ru>
2023-04-20 21:04:10 +0500

    Was created test project.

---
+++ TelegramBot.Tests/RequestToPrivatBankTests.cs
@@ -0,0 +1,23 @@
+using TelegramBot.Model;
+using TelegramBot.Services;
+
+namespace TelegramBot.Tests
+{
+	[TestClass]
+	public class RequestToPrivatBankTests
+	{
+		[TestMethod]
+		public void GetCurrencyRatesAsync_DateWithExistData_26CurrencyRateExpected()
+		{
+			//Arrange
+			RequestToPrivatBank request = new RequestToPrivatBank(new DateOnly(2023, 04, 20));
+			int expectedCurrencyRate = 26;
+
+			//Act
+			int actual = request.GetCurrencyRatesAsync().Result.Count();
+
+			//Assert
+			Assert.AreEqual(expectedCurrencyRate, actual);
+		}
+	}
+}
